19b9eea0a76b782f4d397f4d31582dfb7e580c36
ByeonB <thienhoang.buile@gmail.com>
2024-07-28 15:44:23 +0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,121 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const Username = req.body.username;
+  const Password = req.body.password;
+  if(!Username || !Password){
+    return res.status(404).json({message: "Invalid: username or password not provided"});
+  }else{
+    let user = users.filter((user)=>(
+        user.username === Username
+    ))
+    if(user.length > 0){
+        return res.status(404).json({message: "Invalid: username already exists"});
+    }else{
+        users.push({"username": Username, "password": Password})
+        return res.status(200).json({message: "User registered successfully"});
+    }
+  }
+  
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let myPromise = new Promise((resolve,reject) => {
+        try{
+            let bookList = [];
+            Object.values(books).map((element)=>(
+                bookList.push(element["title"])
+            ))
+            resolve(bookList);
+        }catch(error){
+            reject("Error!")
+        } 
+    })
+    myPromise.then((bookList)=>{
+        return res.send(JSON.stringify(bookList));
+    })
+    
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let myPromise = new Promise((resolve,reject) => {
+        try{
+            let ISBN  = req.params.isbn;
+            const book = books[ISBN];
+            resolve(book);
+        }catch(error){
+            reject("Error occured")
+        } 
+    })
+    myPromise.then((book)=>{
+        if(book){
+            return res.send(JSON.stringify(book))
+        }else{
+            return res.status(404).json({message: "Book doesn't exists with given isbn"})
+        } 
+    })   
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let myPromise = new Promise((resolve,reject) => {
+        try{
+            let Author  = req.params.author.replace(/[:\\"]/g,'');
+            let book =  Object.values(books).filter((element)=>(
+                element["author"]===Author
+            ))
+            resolve(book);
+        }catch(error){
+            reject("Error occured")
+        } 
+    })
+    myPromise.then((book)=>{
+        if(book.length>0){
+            return res.send(JSON.stringify(book))
+        }else{
+            return res.status(404).json({message: "Book doesn't exists"})
+        } 
+    })  
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+        let myPromise = new Promise((resolve,reject) => {
+            try{
+                let Title  = req.params.title.replace(/[:\\"]/g,'');
+                let book =  Object.values(books).filter((element)=>(
+                    element["title"]===Title
+                ))
+                resolve(book);
+            }catch(error){
+                reject("Error occured")
+            } 
+        })
+        myPromise.then((book)=>{
+            if(book.length>0){
+                return res.send(JSON.stringify(book))
+            }else{
+                return res.status(404).json({message: "Book doesn't exists"})
+            } 
+        })  
+    });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let ISBN  = req.params.isbn;
+    if(books[ISBN]){
+        if(books[ISBN]["reviews"].length > 0){
+            return res.send(JSON.stringify(books[ISBN]))
+        }else{
+            return res.status(200).json({message: "No reviews available"})
+        }
+        
+    }else{
+        return res.status(404).json({message: "Book doesn't exists with given ISBN"})
+    }
 });
+
 
 module.exports.general = public_users;
